e9e32b630761cc5448712b0471a54620f5edf073
thePraet0rian404 <Praet0ria404@gmail.com>
2025-07-23 13:52:24 +0200

    changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const { Pool } = require('pg');
