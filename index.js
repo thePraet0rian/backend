e093dd3d96ff434263f6902bffb13938958abad1
thePraet0rian404 <Praet0ria404@gmail.com>
2025-07-23 13:40:42 +0200

    changes

--- index.js
+++ index.js
@@ -1,62 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const FILE_PATH = path.join(__dirname, 'submissions.json');
-
-app.post('/submit', (req, res) => {
-	const { Name, Vorname, Mail} = req.body;
-
-	const newEntry = {
-		Name, 
-		Vorname,
-		Mail,
-	};
-
-	fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-		let submissions = [];
-		if (!err && data) {
-			try {
-				submissions = JSON.parse(data);
-
-			} catch (e) {
-				console.error('Error parsing existing JSON:', e);
-			}
-		}
-
-		submissions.push(newEntry);
-		console.log(submissions);
-
-		fs.writeFile(FILE_PATH, JSON.stringify(submissions, null, 2), err => {
-			res.redirect('https://vini101.github.io');
-		});
-
-	});
+const {CLIENT} = require('pg');
+const client = new CLIENT({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorize: false
+	}
 });
+client.connect();
+
+
+app.post('/submit', async (req, res) => {
+	const {Name, Vorname, Mail} = req.body;
+	// I am not sure if there is a missing semicolon in l 26
+	try {
+		await client.query(
+			'INSERT INTO user_info (name, vorname, mail) VALUES ($1, $2, $3)', 
+			[Name, Vorname, Mail]
+		);
+	} catch (err) {
+		console.error('Error adding new entries to the db.')
+	}
+});
+
 
 app.get('/submission-count', async (req, res) => {
-	fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-		if (!err) {
-		let submissions = JSON.parse(data);
-		console.log('data');
-		console.log(data);
-		console.log(submissions);
-		res.json({ data });
-		} else {
-			const stuff = JSON.stringify([0]);
-			res.json({ stuff });
-		}
-	});
+	try {
+		const result = await client.query('SELECT COUNT(*) FROM user_info');
+		console.log(result);
+		res.json({ result });
+	} catch (err) {
+		console.error('Error counting rows of the table.');
+	}
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
